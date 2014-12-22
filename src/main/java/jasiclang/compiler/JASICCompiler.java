1240484d35fd0909424b730bbb950f858e99eb64
xbony2 <xbony22@gmail.com>
2014-12-22 11:23:50 -0500

    Work

--- src/main/java/jasiclang/compiler/JASICCompiler.java
+++ src/main/java/jasiclang/compiler/JASICCompiler.java
@@ -1,5 +1,30 @@
 package main.java.jasiclang.compiler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import main.java.jasiclang.compiler.exceptions.FileFoundException;
+
 public class JASICCompiler {
 	public static final String EXTENSION = "jasic";
+	
+	public static void compile(File jasicSrc, File compiledClass){
+		try{
+			if(!jasicSrc.exists()){
+				throw new FileNotFoundException("File not found: " + jasicSrc.getName());
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+			return;
+		}
+		
+		try{
+			if(compiledClass.exists()){
+				throw new FileFoundException("The specified output is already in use");
+			}
+		}catch(FileFoundException e){
+			e.printStackTrace();
+			return;
+		}
+	}
 }
---
+++ src/main/java/jasiclang/compiler/exceptions/FileFoundException.java
@@ -0,0 +1,25 @@
+package main.java.jasiclang.compiler.exceptions;
+
+/**
+ * Used when a file is found, when it shouldn't be.
+ * 
+ * @author xbony2
+ *
+ */
+public class FileFoundException extends Exception{
+	public FileFoundException(){
+		super();
+	}
+	
+	public FileFoundException(String message){
+		super(message);
+	}
+	
+	public FileFoundException(Throwable clause){
+		super(clause);
+	}
+	
+	public FileFoundException(String message, Throwable clause){
+		super(message, clause);
+	}
+}
